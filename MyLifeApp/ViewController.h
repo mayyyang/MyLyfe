3baa464ef8649bcf89c32dd1134f5adee13dbaac
May <mayy.yang@gmail.com>
2015-02-02 13:44:37 -0800

    Initial Commit

---
+++ MyLifeApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyLifeApp
+//
+//  Created by May Yang on 2/2/15.
+//  Copyright (c) 2015 May Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
