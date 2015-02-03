c90d3987c3a3a1582e9fb7dcea57c9e23d1de7bb
May <mayy.yang@gmail.com>
2015-02-03 14:15:59 -0800

    EditViewController kind of working now

---
+++ MyLifeApp/EditViewController.m
@@ -0,0 +1,37 @@
+//
+//  EditViewController.m
+//  MyLifeApp
+//
+//  Created by May Yang on 2/3/15.
+//  Copyright (c) 2015 May Yang. All rights reserved.
+//
+
+#import "EditViewController.h"
+
+@interface EditViewController ()
+
+@end
+
+@implementation EditViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
