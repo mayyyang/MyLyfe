c900b47e463ae51d2121f8701de3deea37f1b8ad
May <mayy.yang@gmail.com>
2015-02-03 13:41:42 -0800

    committing before creating branch

---
+++ MyLifeApp/Base.lproj/EditViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditViewController.h
+//  MyLifeApp
+//
+//  Created by May Yang on 2/3/15.
+//  Copyright (c) 2015 May Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditViewController : UIViewController
+
+@end
---
+++ MyLifeApp/Base.lproj/EditViewController.m
@@ -0,0 +1,22 @@
+//
+//  EditViewController.m
+//  MyLifeApp
+//
+//  Created by May Yang on 2/3/15.
+//  Copyright (c) 2015 May Yang. All rights reserved.
+//
+
+#import "EditViewController.h"
+
+@interface EditViewController ()
+@end
+
+@implementation EditViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+
+@end
