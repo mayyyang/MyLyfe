f61815fb76853430a8afc97409f32bbf2231755f
Chakravarthi <ac2349@cornell.edu>
2015-02-15 13:24:01 -0800

    created login view controller

---
+++ MyLifeApp/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  MyLifeApp
+//
+//  Created by Chakravarthi,Abhyuday on 2/15/15.
+//  Copyright (c) 2015 May Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
