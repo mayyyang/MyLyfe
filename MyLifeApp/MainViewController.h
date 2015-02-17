ea5d39da93777414d38ef29a9c73fbd887769e01
May <mayy.yang@gmail.com>
2015-02-16 17:35:37 -0800

    created MainViewController class

---
+++ MyLifeApp/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  MyLifeApp
+//
+//  Created by May Yang on 2/16/15.
+//  Copyright (c) 2015 May Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ MyLifeApp/MainViewController.m
@@ -0,0 +1,37 @@
+//
+//  MainViewController.m
+//  MyLifeApp
+//
+//  Created by May Yang on 2/16/15.
+//  Copyright (c) 2015 May Yang. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
